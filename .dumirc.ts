8a2ff89527d622e5401e9ad78a637b2583186ee4
houlinjiang <houlinjiang@jd.com>
2025-02-26 18:10:00 +0800

    fix docs public path

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'dumi';
 const publicPath = process.env.NODE_ENV === 'production' ? '/lin-lib/' : '/';
 export default defineConfig({
   outputPath: 'lin-lib',
-  history: { type: 'hash' },
   base: publicPath,
   publicPath,
   themeConfig: {
